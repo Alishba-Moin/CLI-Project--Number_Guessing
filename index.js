e0798e18c05c07ba53edf5fad660e64230e4771c
Alishba-Moin <alishbamoin797@gmail.com>
2024-06-24 08:12:48 -0700

    update code

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.yellowBright(`\n<<<========================================>>>`));
+console.log(chalk.bold.redBright(`<<<========>>>  ${chalk.redBright.bold('NUMBER GUESSING GAME')}  <<<========>>>`));
+console.log(chalk.bold.yellowBright(`<<<========================================>>>\n`));
 const random_Number = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number between 1-6:"
+        message: chalk.bold.italic("Please guess a number between 1-6:")
     },
 ]);
 if (answer.userGuessedNumber === random_Number) {
-    console.log("Congratulation! you guessed right number.");
+    console.log(chalk.bold.greenBright("Congratulation! You Guessed Correct Number."));
 }
 else {
-    console.log("You guessed wrong number.");
+    console.log(chalk.bold.redBright("Oppsss! You Guessed Wrong Number"));
 }
